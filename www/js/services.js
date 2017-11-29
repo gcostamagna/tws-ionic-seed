6247285e473234480fa277e8321de7f05814f026
Gabriele Costamagna <gabriele.costamagna@gmail.com>
2017-11-29 11:56:43 +0100

    Test login su tablet (first of many)

--- www/js/services.js
+++ www/js/services.js
@@ -2425,80 +2425,122 @@ service.factory('User', function($http) {
     };
 })
 
-service.factory('Auth', function($rootScope) {
-
-  var Auth0Cordova = require('@auth0/cordova');
-  var auth0 = require('auth0-js');
-  var userProfile = {};
-
-  var auth0Config = {
-    clientId: 'wSyqm1HK0kOJ2APXyS6CJjLkI4Er2oCq',
-    domain: 'thewinesider.eu.auth0.com',
-    callbackURL: location.href,
-    packageIdentifier: 'io.ionic.starter'
-  };
-
-  auth0Config.clientID = auth0Config.clientId;
-
-  var webAuth = new auth0.WebAuth(auth0Config);
-
-  function setSession(authResult) {
-    var expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
-    window.localStorage.setItem('access_token', authResult.accessToken);
-    window.localStorage.setItem('id_token', authResult.idToken);
-    window.localStorage.setItem('expires_at', expiresAt);
-  }
-
-  function isAuthenticated() {
-    var expiresAt = JSON.parse(window.localStorage.getItem('expires_at'));
-    return Date.now() < expiresAt;
-  }
-  
-  function getProfile(cb) {
-    var accessToken = localStorage.getItem('access_token');
-    if (!accessToken) {
-      throw new Error('Access token must exist to fetch profile');
-    }
-
-    webAuth.client.userInfo(accessToken, (err, profile) => {
-      if (profile) {
-        userProfile = profile;
-      }
-      cb(err, profile);
-    });
-  }
-
-  function login() {
-    var client = new Auth0Cordova(auth0Config);
-
-    var options = {
-      scope: 'openid profile offline_access'
+/** 
+    var LoginController = function($scope, $http, App) {
+        var webAuth = new auth0.WebAuth({
+            domain: "thewinesider.eu.auth0.com",
+            clientID: "0bFEhpOfFECLOhlNlabrsv0RTC4aZH3g",
+            responseType: "token",
+        });
+        $scope.login = function(user) {
+            webAuth.client.login({
+                realm: "thewinesider",
+                username: user.id,
+                password: user.password,
+                scope: "openid email profile"
+            }, function(err, authResult) {
+                if(err) {
+                    return console.warn(err);
+                }
+                webAuth.client.userInfo(authResult.accessToken, function(err, userInfo) {
+                    if(err) {
+                        return console.warn(err);
+                    }
+                    var auth0Manage = new auth0.Management({
+                        domain: "thewinesider.eu.auth0.com",
+                        token: authResult.idToken
+                    });
+                    auth0Manage.getUser(window.encodeURIComponent(userInfo.sub), function(err, userInfo) {
+                        if(err) {
+                            return console.warn(err);
+                        }
+                        // Now you have all the user's information
+                        App.user = angular.extend({}, user, authResult, userInfo);
+                    });
+                });
+            });
+        };
     };
 
-    client.authorize(options, function(err, authResult) {
-      if (err) {
-        throw new Error(err);
-      }
-      if (authResult && authResult.accessToken && authResult.idToken) {
-        setSession(authResult);
-        $rootScope.$apply();
-      }
-    });
-  }
+**/
 
-  function logout() {
-    window.localStorage.removeItem('profile');
-    window.localStorage.removeItem('access_token');
-    window.localStorage.removeItem('id_token');
-    window.localStorage.removeItem('expires_at');
-  }
+service.factory('Auth', function($rootScope) {
 
-  return {
-    login: login,
-    logout: logout,
-    getProfile: getProfile,
-    isAuthenticated: isAuthenticated
-  };
+    var Auth0Cordova = require('@auth0/cordova');
+    var auth0 = require('auth0-js');
+    var userProfile = {};
+
+    var auth0Config = {
+        clientId: 'wSyqm1HK0kOJ2APXyS6CJjLkI4Er2oCq',
+        domain: 'thewinesider.eu.auth0.com',
+        callbackURL: location.href,
+        packageIdentifier: 'io.ionic.starter'
+    };
+
+    auth0Config.clientID = auth0Config.clientId;
+
+    var webAuth = new auth0.WebAuth(auth0Config);
+
+    function setSession(authResult) {
+        var expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+        window.localStorage.setItem('access_token', authResult.accessToken);
+        window.localStorage.setItem('id_token', authResult.idToken);
+        window.localStorage.setItem('expires_at', expiresAt);
+    }
+
+    function isAuthenticated() {
+        var expiresAt = JSON.parse(window.localStorage.getItem('expires_at'));
+        return Date.now() < expiresAt;
+    }
+
+    function getProfile(cb) {
+        var accessToken = localStorage.getItem('access_token');
+        if (!accessToken) {
+            throw new Error('Access token must exist to fetch profile');
+        }
+
+        webAuth.client.userInfo(accessToken, (err, profile) => {
+            if (profile) {
+                userProfile = profile;
+            }
+            cb(err, profile);
+        });
+    }
+
+    function login(user) {
+        var client = new Auth0Cordova(auth0Config);
+        console.log("Dopo client");
+        var options = {
+            realm: "thewinesider",
+            username: user.username,
+            password: user.password,
+            scope: 'openid profile offline_access'
+        };
+        console.log("Dopo options");
+        webAuth.client.login(options, function(err, authResult) {
+            if (err) {
+                throw new Error(err);
+            }
+            if (authResult && authResult.accessToken && authResult.idToken) {
+                setSession(authResult);
+                $rootScope.$apply();
+            }
+        });
+    }
+
+    function logout() {
+        window.localStorage.removeItem('profile');
+        window.localStorage.removeItem('access_token');
+        window.localStorage.removeItem('id_token');
+        window.localStorage.removeItem('expires_at');
+    }
+
+    return {
+        login: login,
+        logout: logout,
+        getProfile: getProfile,
+        isAuthenticated: isAuthenticated
+    };
 });
 
 
